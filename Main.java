973d9147f38a879c98002c4e438cd6ada7e74a1b
asysgupta <asysgupta@gmail.com>
2022-06-23 21:42:51 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.albatross.Interfaces;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+    }
+}
